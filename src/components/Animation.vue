8ff52aad3e5447428a641de8065ebc94e89ef807
Jeroen van der Tuin <jeroen@vandertuin.nl>
2022-06-12 23:32:34 -0500

    Add animation.

---
+++ src/components/Animation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="animation" />
+</template>
+
+<style scoped>
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.animation {
+  --size: 500px;
+
+  position: absolute;
+  top: 100px;
+  left: 50%;
+  transform: translateX(600px);
+  width: var(--size);
+  height: var(--size);
+  filter: blur(calc(var(--size) / 5));
+  background-image: linear-gradient(#37eba9d9, #5b37eb);
+  animation: rotate 50s cubic-bezier(0.8, 0.2, 0.2, 0.8) alternate infinite;
+  border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
+  z-index: -1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
+import Animation from '../components/Animation.vue';
 import PageHeader from "@/components/PageHeader.vue"
 import Hero from '@/components/Hero.vue';
 </script>
 
 <template>
+  <Animation />
   <PageHeader />
   <Hero />
 </template>
