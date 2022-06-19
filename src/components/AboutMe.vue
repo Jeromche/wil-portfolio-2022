b558ab79301cbe9902f80843373ef08593d2a189
Jeroen van der Tuin <jeroen@vandertuin.nl>
2022-06-18 22:54:38 -0500

    Add about me section.

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <h2>Resources</h2>
+    <div class="container">
+      <div class="content">
+        <p>Every now and again I create components or widgets on Codepen to experiment how I would make something, to
+          learn how to create the best version or just to pass the time.</p>
+        <p>Here is a selection list of what I've been up to lately. Feel free to copy and use any of it in your own
+          projects.
+        </p>
+      </div>
+      <div class="items" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+h2 {
+  font-family: 'Permanent Marker', cursive;
+  font-size: 20px;
+  line-height: 1.5;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 36px;
+}
+
+.content {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.5;
+  padding-right: 100px;
+}
+
+.content p {
+  margin-bottom: 1em;
+}
+
+.items {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 36px;
+}
+</style>
