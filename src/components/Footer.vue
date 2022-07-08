1a5a04dcd7495051d98bceabcd08be0dd6c4078a
Jeroen van der Tuin <jeroen@vandertuin.nl>
2022-07-07 20:48:50 -0500

    Start working on the footer.

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <slot />
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: #0E00FF;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-import Animation from '../components/Animation.vue';
+import Main from '@/components/Main.vue'
+import Animation from '@/components/Animation.vue';
 import PageHeader from "@/components/PageHeader.vue"
 import Hero from '@/components/Hero.vue';
 import RecentWork from '@/components/RecentWork.vue';
 import Resources from '@/components/Resources.vue';
+import Footer from '@/components/Footer.vue';
 import AboutMe from '@/components/AboutMe.vue';
 </script>
 
 <template>
-  <Animation />
-  <PageHeader />
-  <Hero />
-  <RecentWork />
-  <Resources />
-  <AboutMe />
+  <Main>
+    <Animation />
+    <PageHeader />
+    <Hero />
+    <RecentWork />
+    <Resources />
+  </Main>
+  <Footer>
+    <AboutMe />
+  </Footer>
 </template>
